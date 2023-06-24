ca4acaae157583cebdb95aff3760a1f4e6ea4cf1
Sara Tamer <S320022@ese.gov.ae>
2023-06-24 11:57:46 +0200

    Initial Commit

---
+++ Counter/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter
+//
+//  Created by Sara Tamer on 24/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
